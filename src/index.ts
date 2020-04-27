2935b1198bc0baae06c73a75d54baffc7c7b6d00
Alex Wild <alex.j.wild@gmail.com>
2020-04-27 10:02:50 +0100

    Export readonly functions

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ import decode from './decode';
 import isExpired from './is-expired';
 import sign from './sign';
 
-export default {
+type JWebT = {
+  readonly decode: typeof decode;
+  readonly isExpired: typeof isExpired;
+  readonly sign: typeof sign;
+};
+
+const jWebT: JWebT = {
   decode,
   isExpired,
   sign,
 };
+
+export default jWebT;
