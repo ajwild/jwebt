6af407e4a11bcd4ee29369fe2def1f5b090f88da
Alex Wild <alex.j.wild@gmail.com>
2020-04-27 09:59:45 +0100

    Add isExpired helper

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import decode from './decode';
+import isExpired from './is-expired';
 import sign from './sign';
 
 export default {
   decode,
+  isExpired,
   sign,
 };
---
+++ src/is-expired.test.ts
@@ -0,0 +1,37 @@
+import isExpired from './is-expired';
+import decode from './decode';
+
+jest.mock('./decode');
+const mockedDecode = decode as jest.MockedFunction<typeof decode>;
+
+const fakeJWT = 'abc.def.ghi';
+
+describe('isExpired', () => {
+  test('JWT without exp should not expire', () => {
+    mockedDecode.mockImplementationOnce(() => ({}));
+    expect(isExpired(fakeJWT)).toBe(false);
+  });
+  test('JWT with future exp date should not be expired', () => {
+    const exp = Math.floor(new Date().getTime() / 1000) + 60;
+    mockedDecode.mockImplementationOnce(() => ({ exp }));
+    expect(isExpired(fakeJWT)).toBe(false);
+  });
+  test('JWT with past exp date should be expired', () => {
+    const exp = Math.floor(new Date().getTime() / 1000) - 60;
+    mockedDecode.mockImplementationOnce(() => ({ exp }));
+    expect(isExpired(fakeJWT)).toBe(true);
+  });
+
+  describe('options.expiredWithinSeconds', () => {
+    test('JWT expiring in 61 seconds is not expired when expiredWithinSeconds = 60', () => {
+      const exp = Math.floor(new Date().getTime() / 1000) + 61;
+      mockedDecode.mockImplementationOnce(() => ({ exp }));
+      expect(isExpired(fakeJWT, { expiredWithinSeconds: 60 })).toBe(false);
+    });
+    test('JWT expiring in 59 seconds is expired when expiredWithinSeconds = 60', () => {
+      const exp = Math.floor(new Date().getTime() / 1000) + 59;
+      mockedDecode.mockImplementationOnce(() => ({ exp }));
+      expect(isExpired(fakeJWT, { expiredWithinSeconds: 60 })).toBe(true);
+    });
+  });
+});
